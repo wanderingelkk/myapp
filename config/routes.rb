f48143f18601397d12c868fae648708904f2fb7a
Ryan Sullivan <ryansullivan@Ryans-MacBook-Air.local>
2016-06-10 12:37:19 -0400

    Static Pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'static_pages/home'
+
+  get 'static_pages/help'
+
   devise_for :users, controllers: { sessions: "users/sessions" }
   devise_scope :user do
     authenticated :user do
