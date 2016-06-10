f6b3c771e9427c1175cb4df50acfe135256bc4f8
Ryan Sullivan <ryansullivan@Ryans-MacBook-Air.local>
2016-06-10 12:04:40 -0400

    major changes

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,20 @@
 Rails.application.routes.draw do
   devise_for :users, controllers: { sessions: "users/sessions" }
+  devise_scope :user do
+    authenticated :user do
+      root :to => 'posts#index'
+    end
+    unauthenticated :user do
+      root :to => 'devise/sessions#new', as: :unauthenticated_root
+    end
+  end
+
   resources :posts
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root 'posts#index'
+  # root 'posts#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
